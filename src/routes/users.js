76c93d6fda289a4cfb205ca0221f792b5935967b
Shadee Merhi <shadmerhi@gmail.com>
2021-01-03 23:08:38 -0700

    GET user by email route added

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,6 +2,19 @@ const router = require('express').Router();
 
 module.exports = (db) => {
 
+  router.get('/users/:email', (req, res) => {
+    const email = req.params;
+    console.log('email at endpoint', email);
+    const queryParams = [
+      email
+    ];
+
+    const query = `SELECT * FROM users WHERE email = $1`;
+    db.query(query, queryParams).then(data => {
+      res.send(data.rows);
+    })
+  })
+
   router.post('/users', (req, res) => {
 
     const { uid, email } = req.body.user;
