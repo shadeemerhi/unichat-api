84839e24011284b6679889b0b598bed0d693be8a
Shadee Merhi <shadmerhi@gmail.com>
2021-01-15 08:17:58 -0700

    initial courses inserted

---
+++ src/db/seeds/courses.sql
@@ -0,0 +1,11 @@
+INSERT INTO courses (name, color_code)
+VALUES
+('MATH 211', '#1AE060'),
+('MATH 275', '#49B1FD'),
+('ENGG 201', '#FF5A5F'),
+('ENGG 233', '#FAC537'),
+('ENGG 349', '#5E44F0'),
+('ENGG 311', '#EF9C49'),
+('CHEM 209', '#DE47F2'),
+('ENSF 300','#42EFDB'),
+('MATH 375', '#FF5A5F');
