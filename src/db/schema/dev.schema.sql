c38305a92a62e09ec392418f003ca9d4c1bec2fe
Shadee Merhi <shadmerhi@gmail.com>
2021-01-19 23:52:35 -0700

    added additional fields to courses and created coursesMessages

--- src/db/schema/dev.schema.sql
+++ src/db/schema/dev.schema.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS courses CASCADE;
 DROP TABLE IF EXISTS tutors CASCADE;
 DROP TABLE IF EXISTS programs CASCADE;
-
+DROP TABLE IF EXISTS courses CASCADE;
+DROP TABLE IF EXISTS coursesMessages CASCADE;
 
 
 CREATE TABLE users (
@@ -11,26 +11,36 @@ CREATE TABLE users (
   first_name VARCHAR(255) NOT NULL,
   last_name VARCHAR(255) NOT NULL,
   program VARCHAR(255),
-  is_active boolean DEFAULT true,
-  is_tutor boolean DEFAULT false
+  is_active BOOLEAN DEFAULT true,
+  is_tutor BOOLEAN DEFAULT false
 );
 
 CREATE TABLE courses (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
+  participants INTEGER DEFAULT 0,
+  last_activity TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
   color_main VARCHAR(7),
   color_gradient VARCHAR(7)
 );
 
+CREATE TABLE coursesMessages (
+  id SERIAL PRIMARY KEY NOT NULL,
+  course_id INTEGER REFERENCES courses(id) ON DELETE CASCADE,
+  sender_id VARCHAR(255) REFERENCES users(id) ON DELETE CASCADE,
+  body TEXT,
+  is_edited BOOLEAN DEFAULT false,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
 
 CREATE TABLE tutors (
   id SERIAL PRIMARY KEY NOT NULL,
-  user_id VARCHAR(255) REFERENCES users(id),
+  user_id VARCHAR(255) REFERENCES users(id) ON DELETE CASCADE,
   course_list text[]
 );
 
 CREATE TABLE programs (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL
-)
+);
 
