16d511a9592e644f9f3616da35dedcc058a093ab
Shadee Merhi <shadmerhi@gmail.com>
2021-01-25 16:11:01 -0700

    changed users to Obj, currently not using this file

--- src/helpers/roomUsers.js
+++ src/helpers/roomUsers.js
@@ -1,5 +1,6 @@
 const removeUserFromRoom = function(users, userIdToRemove) {
-  return users.filter(u => u.user.uid !== userIdToRemove);
+  // return users.filter(u => u.user.uid !== userIdToRemove);
+  return delete users[userIdToRemove];
 }
 
 module.exports = { removeUserFromRoom }
