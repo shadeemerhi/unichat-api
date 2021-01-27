142324c6ae9ba88458f4107a55f60ee353b91e89
Shadee Merhi <shadmerhi@gmail.com>
2021-01-27 00:08:36 -0700

    message routes has access to helpers, helpers have access to db

--- src/routes/messages.js
+++ src/routes/messages.js
@@ -2,6 +2,11 @@ const router = require('express').Router();
 
 module.exports = (helpers) => {
 
+  router.get('/messages', (req, res) => {
+    console.log('at the messages endpoint');
+    res.send('from messages endpoint');
+  })
+
 
   return router;
 }
