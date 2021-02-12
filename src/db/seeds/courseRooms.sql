cf7d8294e8485a8ec00ca9335d2047b02e6d09dc
Shadee Merhi <shadmerhi@gmail.com>
2021-02-11 19:36:50 -0700

    table name changed to courseRooms

---
+++ src/db/seeds/courseRooms.sql
@@ -0,0 +1,11 @@
+INSERT INTO courseRooms (name, color_main, color_gradient)
+VALUES
+('MATH 211', '#1AE060', '#A2F2BF'),
+('MATH 275', '#49B1FD', '#D0E7F8'),
+('ENGG 201', '#FF5A5F','#FFC9CA'),
+('ENGG 233', '#FAC537', '#F8E1A1'),
+('ENGG 349', '#5E44F0', '#BAB0F4'),
+('ENGG 311', '#EF9C49','#F7D0A9'),
+('CHEM 209', '#DE47F2', '#F2B4FA'),
+('ENSF 300','#42EFDB', '#BFFAF3'),
+('MATH 375', '#FF5A5F', '#FFC9CA');
