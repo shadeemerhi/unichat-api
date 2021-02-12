6cc603cb5bc6cffc5d5ef409b2199cd69015dae8
Shadee Merhi <shadmerhi@gmail.com>
2021-02-12 14:48:44 -0700

    getTutorCourses, getCoursesByYear, getCoursesBySubject written

--- src/helpers/courses.js
+++ src/helpers/courses.js
@@ -1,21 +1,65 @@
 module.exports = (db) => {
 
-  const getTutorCourses = () => {
-    
+  const getTutorCourses = async () => {
+    const years = {1: 'first_year', 2: 'second_year', 3: 'third_year'};
+    const courses = {};
+    const coursePromises = [];
+    for(const year in years) {
+      coursePromises.push(getCoursesByYear(year));
+    }
+
+    await Promise.all(coursePromises).then(allCourses => {
+
+      /* 
+        Formatting the courses object to appear as --> 
+        courses = {
+          first_year = {
+            sortCoursesBySubject(allCourses[year - 1]) <-- index is (year - 1) to account for allCourses index
+                                                           starting at 0, where years starts at 1
+          }
+        }
+      */
+      for(const year in years) {
+        courses[years[year]] = sortCoursesBySubject(allCourses[year - 1]);
+      }
+    })
+    .catch(error => console.log(error));
+
+    return courses;
   }
 
-  const getCoursesByYear = (year) => {
+  // courses is optional as we can either filter a passed list or directly query the db for the list
+  const getCoursesByYear = (year, courses = null) => {
+    const query = 
+    `
+    SELECT * FROM courseItems
+    WHERE year = ${year};
+    `;
+
+    return db.query(query).then(data => data.rows);
 
   }
 
-  const getCoursesBySubject = (subject) => {
+  const sortCoursesBySubject = (courses) => {
+    const coursesBySubject = {};
+
+    for(const course of courses) {
+      if(!coursesBySubject[course.subject]) {
+        coursesBySubject[course.subject] = [course];
+      } else {
+        coursesBySubject[course.subject].push(course);
+      }
+    }
+    return coursesBySubject;
 
   }
 
-  return { getCourses };
+  return { getTutorCourses };
 }
 
-// Call helper function to organize courses into proper format
+
+
+// This needs to be added to the README.md
 /* 
   courses = {
     first_year = {
