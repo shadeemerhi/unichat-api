c803edac67930f51b218378bc89586ae1d7c9031
Shadee Merhi <shadmerhi@gmail.com>
2021-02-12 12:24:09 -0700

    course initial helper function skeletons

---
+++ src/helpers/courses.js
@@ -0,0 +1,52 @@
+module.exports = (db) => {
+
+  const getTutorCourses = () => {
+    
+  }
+
+  const getCoursesByYear = (year) => {
+
+  }
+
+  const getCoursesBySubject = (subject) => {
+
+  }
+
+  return { getCourses };
+}
+
+// Call helper function to organize courses into proper format
+/* 
+  courses = {
+    first_year = {
+      MATH: [
+        {
+          name: 'MATH 275',
+          title: 'Calculus for Engineers and Scientists',
+          year: 1
+        },
+        {
+          name: 'MATH 211',
+          title: 'Linear Methods',
+          year: 1
+        }
+      ],
+      ENGG: [
+        {
+          name: 'ENGG 201',
+          title: 'Solids, Liquids, and Gases',
+          year: 1
+        }
+      ]
+    },
+    second_year = {
+      MATH: [
+        {
+          name: 'MATH 375',
+          title: 'Differential Equations',
+          year: 2
+        }
+      ]
+    }
+  }
+*/
