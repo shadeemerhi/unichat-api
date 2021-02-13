b3741117f4b87c167f6bfdf1faecde6ac6b0e15f
Shadee Merhi <shadmerhi@gmail.com>
2021-02-12 17:09:15 -0700

    error handling implemented

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -3,15 +3,22 @@ const router = require('express').Router();
 module.exports = (helpers) => {
 
   router.get('/courses/rooms', (req, res) => {
-    const query = 'SELECT * FROM courseRooms';
-    db.query(query).then(data => {
-      res.send(data.rows);
-    });
+    helpers
+      .getCourseRooms()
+      .then(data => res.send(data))
+      .catch(error => {
+        res.status(500).send({ error })
+      });
   });
 
   // Will be hit by 'Become a Tutor' components
   router.get('/courses/tutorcourses', (req, res) => {
-
+    helpers
+      .getTutorCourses()
+      .then(data => res.send(data))
+      .catch(error => {
+        res.status(500).send({ error })
+      });
   })
 
 
