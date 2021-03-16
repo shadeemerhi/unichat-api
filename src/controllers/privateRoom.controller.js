d1155b0cb7d57e308d0281ab27c2d5e59b2d0053
Vladyslav <kytsevolov@gmail.com>
2021-03-15 19:16:31 -0600

    Created private room controller

---
+++ src/controllers/privateRoom.controller.js
@@ -0,0 +1,20 @@
+const createPrivateRoom = (db) => async (req, res) => {
+    const query = `INSERT INTO privateRooms (name, author_id) VALUES ($1, $2) RETURNING *`;
+
+    if(req.body) {
+        const {name: roomName, userId: authorId } = req.body;
+
+        const queryParams = [roomName, authorId];
+
+        try {
+           const {rows} = await db.query(query, queryParams);
+           res.json({msg: 'room created', room: rows});
+        } catch (e) {
+            res.status(500).json({msg: 'Something went wrong on private room creation'})
+        }
+
+    }
+}
+
+
+module.exports = { createPrivateRoom }
