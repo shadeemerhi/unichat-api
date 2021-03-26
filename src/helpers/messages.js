093f0b19a919806ed79470dcf73bc4dca83f7b8b
Vladyslav <kytsevolov@gmail.com>
2021-03-25 19:03:55 -0600

    created helpers to create and fetch private msg

--- src/helpers/messages.js
+++ src/helpers/messages.js
@@ -1,43 +1,52 @@
+const { createPrivateRoomMsg, getPrivateRoomMsg } = require("./privateRoom");
+
 module.exports = (db) => {
 
   const createMessage = (course_id, sender_id, body) => {
+    if(course_id.length > 5) {
+      return createPrivateRoomMsg(course_id, sender_id, body, db);
+    } else {
+      const queryParams = [
+        course_id,
+        sender_id,
+        body
+      ];
 
-    const queryParams = [
-      course_id,
-      sender_id,
-      body
-    ];
+      const query =
+        `
+        INSERT INTO courseRoomsMessages (course_id, sender_id, body)
+        VALUES ($1, $2, $3)
+        RETURNING id, course_id, sender_id, body, is_edited, 
+        TRIM(LEADING '0' FROM to_char(created_at, 'HH12:MI AM')) as created_at;`;
 
-    const query = 
-      `
-      INSERT INTO courseRoomsMessages (course_id, sender_id, body)
-      VALUES ($1, $2, $3)
-      RETURNING id, course_id, sender_id, body, is_edited, 
-      TRIM(LEADING '0' FROM to_char(created_at, 'HH12:MI AM')) as created_at;`;
-
-    return db.query(query, queryParams);
+      return db.query(query, queryParams).catch((e) => console.log('failed to create msg ', e))
+    }
   }
 
-  const getMessagesInRoom = (room_id) => {
+  const getMessagesInRoom = async (room_id) => {
+    if(room_id.length > 5) {
+      return getPrivateRoomMsg(room_id, db)
+    } else {
+      const queryParams = [
+        room_id
+      ];
 
-    const queryParams = [
-      room_id
-    ];
+      const query =
+      `
+      SELECT courseRoomsMessages.id, course_id, sender_id, body, "firstName", 
+      "lastName", is_edited, TRIM(LEADING '0' FROM to_char(created_at, 'HH12:MI AM')) as created_at 
+      FROM courseRoomsMessages
+      JOIN users ON sender_id = users.id
+      WHERE course_id = $1
+      ORDER BY id;
+      `;
 
-    const query = 
-    `
-    SELECT courseRoomsMessages.id, course_id, sender_id, body, "firstName", 
-    "lastName", is_edited, TRIM(LEADING '0' FROM to_char(created_at, 'HH12:MI AM')) as created_at 
-    FROM courseRoomsMessages
-    JOIN users ON sender_id = users.id
-    WHERE course_id = $1
-    ORDER BY id;
-    `;
+      return db
+        .query(query, queryParams)
+        .then(res => res.rows)
+        .catch(error => console.log(error));
+    }
 
-    return db
-      .query(query, queryParams)
-      .then(res => res.rows)
-      .catch(error => console.log(error));
   }
 
   const editMessage = (id, newBody) => {
@@ -47,7 +56,7 @@ module.exports = (db) => {
       id
     ];
 
-    const query = 
+    const query =
     `
     UPDATE courseRoomsMessages
     SET 
